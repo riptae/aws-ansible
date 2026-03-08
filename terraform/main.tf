318321823ffe3d27a02821e1b883dc10e91f5b56
yun <riptaggart@naver.com>
2026-03-08 10:03:09 +0900

    key path modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,15 +86,15 @@ resource "aws_security_group" "ec2_sg" {
 
 resource "aws_key_pair" "ec2_key" {
   key_name   = "t-key"
-  public_key = file("./t-key.pub")
+  public_key = file("${path.module}/../t-key.pub")
 }
+
 
 resource "aws_instance" "ec2" {
   ami           = "ami-0e735aba742568824"
   instance_type = "t3.micro"
 
   subnet_id = aws_subnet.public_a.id
-
 
   vpc_security_group_ids = [
     aws_security_group.ec2_sg.id
@@ -109,4 +109,3 @@ resource "aws_instance" "ec2" {
 
 
 }
-
