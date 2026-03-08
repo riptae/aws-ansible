76ef314bee3e96575725f067adf29b2d40cad4b6
yun <riptaggart@naver.com>
2026-03-08 19:56:27 +0900

    os ver upgraded for pythin 3.8 -> 3.10 for ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,14 +89,28 @@ resource "aws_key_pair" "ec2_key" {
   public_key = file("${path.module}/../t-key.pub")
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-0e735aba742568824"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   subnet_id = aws_subnet.public_a.id
   key_name = aws_key_pair.ec2_key.key_name
-  
+
   vpc_security_group_ids = [
     aws_security_group.ec2_sg.id
   ]
